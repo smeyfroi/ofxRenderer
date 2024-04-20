0607fcb050c7c120879dce7db8d6bc3a096254e4
smeyfroi <steve.meyfroidt@gmail.com>
2024-04-20 17:11:31 +0100

    Simplify AddTexture

--- src/shaders/AddTextureShader.h
+++ src/shaders/AddTextureShader.h
@@ -3,16 +3,20 @@
 #include "Shader.h"
 
 // Handles different-sized addedTexture
-// Clamps to [-1, 1]
+// Clamps values to [-1, 1]
 class AddTextureShader : public Shader {
 
 public:
-  void render(const ofFbo& fbo_, const ofFbo& addedTexture_, float multiplier_) {
+  void render(const ofFbo& fbo_, const ofFbo& addedTexture_, float multiplier_, float textureScale_ = 0.0) {
     ofEnableBlendMode(OF_BLENDMODE_DISABLED);
     shader.begin();
     shader.setUniformTexture("addedTexture", addedTexture_.getTexture(), 1);
     shader.setUniform1f("multiplier", multiplier_);
-    shader.setUniform1f("textureScale", fbo_.getWidth()/addedTexture_.getWidth());
+    if (textureScale_ == 0.0) {
+      shader.setUniform1f("textureScale", fbo_.getWidth()/addedTexture_.getWidth());
+    } else {
+      shader.setUniform1f("textureScale", textureScale_);
+    }
     fbo_.draw(0, 0);
     shader.end();
   }
@@ -25,13 +29,15 @@ protected:
                 uniform sampler2D addedTexture;
                 uniform float multiplier;
                 uniform float textureScale;
+                uniform float offsetX;
+                uniform float offsetY;
 
                 void main() {
                   vec2 xy = gl_TexCoord[0].st;
                   vec4 color = texture2D(tex0, xy);
-                  vec2 xy1 = xy * textureScale;
+                  vec2 xy1 = xy / textureScale;
                   vec4 addedColor = texture2D(addedTexture, xy1) * multiplier;
-                  gl_FragColor = clamp(color + addedColor, vec4(-1.0, -1.0, 0.0, 0.0), vec4(1.0, 1.0, 0.0, 0.0)); // ***** NOTE CLAMP TO KEEP FLUID SIM STABLE ALSO KEEPS MEMORY OVERLAYS WORKING
+                  gl_FragColor = clamp(color + addedColor, vec4(-1.0), vec4(1.0)); // ***** NOTE CLAMP TO KEEP FLUID SIM STABLE
                 }
                 );
   }
