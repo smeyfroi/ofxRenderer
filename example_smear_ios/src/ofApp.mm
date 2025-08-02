b223c4705058a9ff5539e3c7b3e043df8fb9fda5
smeyfroi <steve.meyfroidt@gmail.com>
2025-08-02 10:05:45 +0100

    Tweaks

--- example_smear_ios/src/ofApp.mm
+++ example_smear_ios/src/ofApp.mm
@@ -5,7 +5,7 @@ void ofApp::setup(){
   ofBackground(50);
   ofEnableAlphaBlending();
   
-  fbo.allocate(ofGetWindowWidth(), ofGetWindowHeight(), GL_RGBA); // or 16F, 32F
+  fbo.allocate(ofGetWindowWidth()/4, ofGetWindowHeight()/4, GL_RGBA); // or 16F, 32F
   ofSetColor(0);
   fbo.getSource().begin();
   ofDrawRectangle(0, 0, fbo.getWidth(), fbo.getHeight());
@@ -19,12 +19,12 @@ void ofApp::update(){
   fbo.getSource().begin();
   {
     ofEnableBlendMode(OF_BLENDMODE_DISABLED);
-    ofSetColor(ofFloatColor(ofRandom(1.0), ofRandom(1.0), ofRandom(1.0), 1.0));
-    ofDrawCircle(ofRandomWidth(), ofRandomHeight(), 40.0);
+    ofSetColor(ofFloatColor(ofRandom(1.0), ofRandom(1.0), ofRandom(1.0), ofRandom(1.0)));
+    ofDrawCircle(ofRandomWidth(), ofRandomHeight(), ofRandom(40.0));
   }
   fbo.getSource().end();
 
-  smearShader.render(fbo, {0.0, 0.001}, 0.2);
+  smearShader.render(fbo, {0.0, 0.0005}, 0.4);
 }
 
 //--------------------------------------------------------------
@@ -32,7 +32,7 @@ void ofApp::draw(){
   ofEnableBlendMode(OF_BLENDMODE_DISABLED);
   ofSetColor(255);
 //  smearShader.render();
-  fbo.draw(0, 0);
+  fbo.draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
   ofDrawBitmapString(ofToString(ofGetFrameRate(), 2) + " FPS", 10, 20);
 }
 
