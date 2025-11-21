ff4328997258cf60998b68b51ce1737c351ecbca
smeyfroi <steve.meyfroidt@gmail.com>
2025-11-21 13:29:33 +0000

    Update to OpenGL4.1

--- src/shaders/AddTextureShader.h
+++ src/shaders/AddTextureShader.h
@@ -8,59 +8,36 @@
 class AddTextureShader : public Shader {
 
 public:
-  void render(PingPongFbo& fbo_, const ofTexture& addedTexture_, float multiplier_) {
+  void render(PingPongFbo& fbo_, const ofTexture& addedTexture_, float multiplier_, float offset_ = 0.0f) {
+    ofEnableBlendMode(OF_BLENDMODE_DISABLED);
     fbo_.getTarget().begin();
     shader.begin();
     shader.setUniformTexture("addedTexture", addedTexture_, 1);
     shader.setUniform1f("multiplier", multiplier_);
+    shader.setUniform1f("offset", offset_);
     fbo_.getSource().draw(0, 0);
     shader.end();
     fbo_.getTarget().end();
     fbo_.swap();
   }
   
-  void render(PingPongFbo& fbo_, const ofFbo& addedTexture_, float multiplier_) {
-    fbo_.getTarget().begin();
-    shader.begin();
-    shader.setUniformTexture("addedTexture", addedTexture_.getTexture(), 1);
-    shader.setUniform1f("multiplier", multiplier_);
-    fbo_.getSource().draw(0, 0);
-    shader.end();
-    fbo_.getTarget().end();
-    fbo_.swap();
-  }
-  
-  // Is this for adding the crystals? The scaling is wrong because we're in normalised coords!
-//  void render(const ofFbo& fbo_, const ofFbo& addedTexture_, float multiplier_, float textureScale_ = 0.0) {
-//    ofEnableBlendMode(OF_BLENDMODE_DISABLED);
-//    shader.begin();
-//    shader.setUniformTexture("addedTexture", addedTexture_.getTexture(), 1);
-//    shader.setUniform1f("multiplier", multiplier_);
-//    if (textureScale_ == 0.0) {
-//      shader.setUniform1f("textureScale", fbo_.getWidth()/addedTexture_.getWidth());
-//    } else {
-//      shader.setUniform1f("textureScale", textureScale_);
-//    }
-//    fbo_.draw(0, 0);
-//    shader.end();
-//  }
-
 protected:
-
   std::string getFragmentShader() override {
     return GLSL(
                 uniform sampler2D tex0;
                 uniform sampler2D addedTexture;
                 uniform float multiplier;
-//                uniform float textureScale;
-//                uniform float offsetX;
-//                uniform float offsetY;
+                uniform float offset;
+
+                in vec2 texCoordVarying;
+                out vec4 fragColor;
 
                 void main() {
-                  vec2 xy = gl_TexCoord[0].st;
-                  vec4 color = texture2D(tex0, xy);
-                  vec4 addedColor = texture2D(addedTexture, xy) * multiplier;
-                  gl_FragColor = clamp(color + addedColor, vec4(-1.0), vec4(1.0)); // ***** NOTE CLAMP TO KEEP FLUID SIM STABLE
+                  vec2 xy = texCoordVarying.xy;
+                  vec4 color = texture(tex0, xy);
+                  vec4 rawAddedColor = texture(addedTexture, xy);
+                  vec4 addedColor = rawAddedColor * multiplier + vec4(offset);
+                  fragColor = clamp(color + addedColor, vec4(-1.0), vec4(1.0));
                 }
                 );
   }
