f4773c7d592af32686b3af8309f0efb0f87867c9
smeyfroi <steve.meyfroidt@gmail.com>
2025-12-07 19:51:13 +0000

    Add missing includes

--- src/Shader.h
+++ src/Shader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include "ofMain.h"
 #include "PingPongFbo.h"
 
--- src/fluid/FluidSimulation.h
+++ src/fluid/FluidSimulation.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include "ofxGui.h"
 #include "PingPongFbo.h"
 #include "AdvectShader.h"
